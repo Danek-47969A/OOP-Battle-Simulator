12935472b79808aa4ad8514a7b9b412ca9c5a909
Danek-47969A <143437802+Danek-47969A@users.noreply.github.com>
2026-09-14 09:12:33 -0500

    Added Hero

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 from goblin import Goblin
+from hero import Hero
 
 
 ARENA_NAME = "The Iron Icosahedron"
@@ -16,6 +17,17 @@ def main():
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
     print(f"{goblin2.name} enters the arena with {goblin2.health} health.")
     print("But no hero has answered the call... yet.")
+    hero = Hero("The One Who Knocks")
+    print(f"{hero.name} enters the arena with {hero.health} health.")
+
+    hero_attack_damage = hero.attack()
+    goblin.take_damage(hero_attack_damage)
+    print(f"{hero.name} attacks {goblin.name} for {hero_attack_damage} damage, bringing the goblin to {goblin.health} health")
+
+    if goblin.is_alive:
+        goblin_attack_damage = goblin.attack()
+        hero.take_damage(goblin_attack_damage)
+        print(f"{goblin.name} attacks {hero.name} for {goblin_attack_damage} damage, bringing the hero to {hero.health} health")
 
 
 if __name__ == "__main__":
--- hero.py
+++ hero.py
@@ -1,4 +1,26 @@
+import random
+
 class Hero:
     """The hero blueprint will be implemented later in the project."""
+    def __init__(self, name):
+        self.name = name
+        self.health = 125
+        self.attack_power = 20
+
+    def attack(self):
+        critical_chance = 10
+        crit = random.randint(1,100)
+        if crit < critical_chance:
+            return random.randint(self.attack_power, 2*self.attack_power)
+        else:
+            return random.randint(1, self.attack_power)
+
+    def take_damage(self, damage):
+        self.health=max(0, self.health - damage)
+        print(f"{self.name} takes {damage} damage. Health: {self.health}")
+
+    def is_alive(self):
+        return self.health > 0
+
 
     pass
