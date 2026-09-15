473d6e9aa93405c3f46517599b97f245ca4ff759
Danek-47969A <143437802+Danek-47969A@users.noreply.github.com>
2026-09-15 08:56:00 -0500

    Update game.py

--- game.py
+++ game.py
@@ -4,6 +4,16 @@ from hero import Hero
 
 ARENA_NAME = "The Iron Icosahedron"
 
+def battle(hero: Hero, enemy: Goblin):
+    while hero.is_alive() and enemy.is_alive():
+        hero.damage = hero.attack()
+        enemy.take_damage(hero.is_alive)
+        if enemy.is_alive():
+            enemy.damage = enemy.attack()
+            hero.take_damage(enemy.damage)
+
+        if hero.is_alive():
+            print(f"{enemy.name} attacks {hero.name} for {enemy.damage} damage, bringing the hero to {hero.health} health")
 
 def main():
     """Open the arena and introduce its first opponent."""
@@ -17,17 +27,12 @@ def main():
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
     print(f"{goblin2.name} enters the arena with {goblin2.health} health.")
     print("But no hero has answered the call... yet.")
-    hero = Hero("The One Who Knocks")
+    hero = Hero("The One Who Battles")
     print(f"{hero.name} enters the arena with {hero.health} health.")
 
-    hero_attack_damage = hero.attack()
-    goblin.take_damage(hero_attack_damage)
-    print(f"{hero.name} attacks {goblin.name} for {hero_attack_damage} damage, bringing the goblin to {goblin.health} health")
+    battle(hero, goblin)
+    
 
-    if goblin.is_alive:
-        goblin_attack_damage = goblin.attack()
-        hero.take_damage(goblin_attack_damage)
-        print(f"{goblin.name} attacks {hero.name} for {goblin_attack_damage} damage, bringing the hero to {hero.health} health")
 
 
 if __name__ == "__main__":
